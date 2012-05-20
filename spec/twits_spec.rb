235420a1ee1a08b3cd9159a43b7bedecfb484e12
Bala Paranj <bparanj@gmail.com>
2012-05-20 05:40:00 -0700

    Abuse of mocks. Spec is coupled to the implementation of the method. Spec is brittle. It will break even when the behavior does not change but when the implementation changes. That is likely to happen when you upgrade Twitter gem

--- spec/twits_spec.rb
+++ spec/twits_spec.rb
@@ -9,13 +9,20 @@ describe "Twitter User" do
     end
     
     it "provides the last five tweets from twitter" do
-      tweets = ["race day! http://t.co/nHVyd7s3 #fb",
-              "backed \"Roominate: Make It Yours!\" [toy to inspire next gen of female tech innovators] on @Kickstarter: http://t.co/koMadie2 #fb",
-              "just drove the route for the 5K I'm running in tomorrow: Serious (for IL) hills, ouch, gonne be tough: http://t.co/nHVyd7s3 #fb",
-              "RT @dgroo: Son is declaring that the Honey Badger is his second favorite animal. Must resist urge to tell him 'Honey Badger don't give a shit.'",
-              "RT @Zen_Moments: If you want to sail your ship in a different direction, you must turn one degree at a time. ~ Brian Tracy"]
+      tweets = [
+        {text: 'tweet1'},
+        {text: 'tweet2'},
+        {text: 'tweet3'},
+        {text: 'tweet4'},
+        {text: 'tweet5'},
+        ]
       
-      @user.last_five_tweets.should == tweets 
+      mock_client = mock('client')
+      mock_client.should_receive(:per_page).with(5).and_return(mock_client)
+      mock_client.should_receive(:from).with('logosity').and_return(tweets)
+      Twitter::Search.should_receive(:new).and_return(mock_client)
+      
+      @user.last_five_tweets.should == %w{tweet1 tweet2 tweet3 tweet4 tweet5} 
     end
     
   end
